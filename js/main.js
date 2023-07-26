47d97142cd11eafcbf3959d1ab1f3bc53e45aaae
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-07-26 16:41:15 +0200

    add: Snack 1, Snack 2, Snack 3

---
+++ js/main.js
@@ -0,0 +1,74 @@
+//  Snack 1
+
+// // Richiedo il primo numero
+// const numberFirst = parseInt(prompt("Inserisci il primo numero"));
+// console.log("Primo numero: " + numberFirst);
+
+// // Richiedo il primo numero
+// const numberSecond = parseInt(prompt("Inserisci il secondo numero"));
+// console.log("Secondo numero: " + numberSecond);
+
+// let numberRandom;
+
+// // Genero il numero casuale
+// if (numberFirst > numberSecond) {
+//   numberRandom =
+//     Math.floor(Math.random() * (numberFirst - numberSecond)) + numberSecond;
+// } else if (numberFirst < numberSecond) {
+//   numberRandom =
+//     Math.floor(Math.random() * (numberSecond - numberFirst)) + numberFirst;
+// } else {
+//   numberRandom = numberFirst;
+// }
+
+// // Stampo il numero casuale
+// console.log(numberRandom);
+
+//  ------------------------------------------------------------------- //
+
+// Snack 2
+
+// // Chiedo all'utente la prima parola
+// const firstWord = prompt("Scrivi una parola");
+// console.log("Prima parola: " + firstWord);
+
+// // Chiedo all'utente la seconda parola
+// const secondWord = prompt("Scrivi una parola");
+// console.log("Seconda parola: " + secondWord);
+
+// let printWord;
+
+// // Verifico quale parola è più lunga
+// if (firstWord.length > secondWord.length) {
+//   printWord = firstWord;
+// } else if (firstWord.length < secondWord.length) {
+//   printWord = secondWord;
+// } else {
+//   printWord = firstWord + " " + secondWord;
+// }
+
+// // Stampo la parola
+// console.log(printWord);
+
+//  ------------------------------------------------------------------- //
+
+// Snack 3
+
+// - Creo una lista vuoto
+const listNumbers = [];
+
+let i = 0;
+
+// - la lista non è minore di 50
+while (i < 50) {
+  // - chiedo all'utente dei valori
+  let number = parseInt(prompt("Inserisci un numero"));
+  console.log("numero scritto: " + number);
+
+  i = i + number;
+
+  // - aggiungo quel valore come elemento dentro la lista
+  listNumbers.push(number);
+}
+
+console.table(listNumbers);
---
+++ readme.md
@@ -0,0 +1,55 @@
+#
+
+## Snack 1
+
+### Traccia
+
+- Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
+
+### Istruzioni
+
+- chiedo all'utente "_numero primo_"
+
+- chiedo all'utente "_numero secondo_"
+
+- genero un numero casuale
+  - numero casuale che è tra "_numero primo_" e "_numero secondo_"
+
+## Snack 2
+
+### Traccia
+
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+### Istruzioni
+
+- chiedo l'utente di scriver la "_prima parola_"
+
+- chiedo l'utente di scriver la "_seconda parola_"
+
+- **SE** la lunghezza "_prima parola_" è > alla lunghezza della "_seconda parola_"
+
+  - stampo "_prima parola_"
+
+- **ALTRIMENTI SE** la lunghezza "_seconda parola_" è > alla lunghezza della "_prima parola_"
+
+  - stampo "_seconda parola_"
+
+- **ALTRIMENTI SE** lunghezza della "_prima parola_" **=** alla lunghezza della "_seconda parola_"
+  - stampo "_prima parola_" e "_seconda parola_"
+
+## Snack 3
+
+### Traccia
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+### Istruzioni
+
+- Creo una lista vuoto
+
+- chiedo all'utente dei valori
+
+- aggiungo quel valore come elemento dentro la lista
+
+- **Finche** la lista non è minore di 50
